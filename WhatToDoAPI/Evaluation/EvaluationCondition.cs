d51baef59e9a9e7b319c4a0e0c74a8808d2f85f2
will allen <william.allen1296@gmail.com>
2019-08-30 17:37:11 -0500

    Initial Release

--- WhatToDoAPI/Evaluation/EvaluationCondition.cs
+++ WhatToDoAPI/Evaluation/EvaluationCondition.cs
@@ -5,13 +5,8 @@ using System.Threading.Tasks;
 
 namespace WhatToDoAPI.Evaluation
 {
-    public class EvaluationCondition
+    struct ConditionNameList
     {
-        public string Name { get; set; }
-        public string Preference { get; set; }
-        public string Type { get; set; }
-        public int Rank { get; set; }
-        public string ActualValue { get; set; }
-        public string EvaluationType { get; set; }
+        
     }
 }
